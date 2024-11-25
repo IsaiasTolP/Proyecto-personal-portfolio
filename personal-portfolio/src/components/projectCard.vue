875a44654295dd02af1b6472222020fc9a9aa742
IsaiasTolP <toledop.isaias@gmail.com>
2024-11-25 15:36:52 +0000

    Se sube proyectos con problemas

---
+++ personal-portfolio/src/components/projectCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <div class="card-header">{{ $t('data.title') }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Card',
+        props: {
+            data: {
+                type: Object,
+                required: true
+            }
+        }
+    };
+</script>
---
+++ personal-portfolio/src/views/projects.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-4" v-for="(project, key) in projectList" :key="key">
+                <Card :data="project"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Card from '../components/projectCard.vue';
+    
+    export default {
+        name: "Projects",
+        components: { Card },
+        computed: {
+            projectList() {
+                const projects = this.$t('projects.data');
+                return Object.keys(projects)
+                    .filter(key => key.startsWith('project_'))
+                    .map(key => projects[key]);
+            }
+        }
+    };
+</script>
