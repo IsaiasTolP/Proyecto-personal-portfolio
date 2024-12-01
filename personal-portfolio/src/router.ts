730b4c31534ff00aa8805671c7532809d7812759
IsaiasTolP <toledop.isaias@gmail.com>
2024-12-01 23:09:05 +0000

    Se termina el proyecto

--- personal-portfolio/src/router.ts
+++ personal-portfolio/src/router.ts
@@ -4,6 +4,7 @@ import About from './views/about.vue';
 import Projects from './views/projects.vue';
 import Skills from './views/skills.vue';
 import Experience from './views/experience.vue';
+import Contact from './views/contact.vue';
 
 const routes = [
     { path: '/', component: Home },
@@ -11,6 +12,7 @@ const routes = [
     { path: '/projects', component: Projects},
     { path: '/skills', component: Skills},
     { path: '/experience', component: Experience},
+    { path: '/contact', component: Contact},
 ];
 
 const router = createRouter({
--- personal-portfolio/src/views/experience.vue
+++ personal-portfolio/src/views/experience.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="container">
+    <div class="container my-4 text-center">
+        <a href="/documents/Isaias-CV.pdf" download="Isaias-CV.pdf" class="btn btn-primary mb-4">{{ $t('experience.download') }}</a>
+        <h3 class="mb-3">📅 My timeline</h3>
         <p>{{ $t('experience.data.exp_1') }}</p>
         <p>{{ $t('experience.data.exp_2') }}</p>
         <p>{{ $t('experience.data.exp_3') }}</p>
-        <a href="assets/documents/Isaias-CV.pdf" download="Isaias-CV.pdf" class="btn btn-primary"></a>
     </div>
 </template>
 
